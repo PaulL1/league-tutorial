97858c214bbc3b36a190d31ea03ef9f78d736674
Paul Lambert <paul@planar.id.au>
2013-09-26 21:13:59 +1000

    Tutorial 4: adding ngGrid and a modal edit dialog

--- build.config.js
+++ build.config.js
@@ -67,7 +67,10 @@ module.exports = {
       'vendor/placeholders/angular-placeholders-0.0.1-SNAPSHOT.min.js',
       'vendor/angular-ui-router/release/angular-ui-router.js',
       'vendor/angular-ui-utils/modules/route/route.js',
-      'vendor/angular-resource/angular-resource.js'    ],
+      'vendor/angular-resource/angular-resource.js',
+      'vendor/jquery/jquery.js',
+      'vendor/angular-grid/build/ng-grid.js'
+    ],
     css: [
     ],
     assets: [
--- src/app/club/club.js
+++ src/app/club/club.js
@@ -3,7 +3,8 @@
  */
 angular.module( 'league.club', [
   'ui.state',
-  'ngResource'
+  'ngResource',
+   'ngGrid'
 ])
 
 /**
@@ -25,14 +26,50 @@ angular.module( 'league.club', [
 /**
  * And of course we define a controller for our route.
  */
-.controller( 'ClubCtrl', function ClubController( $scope, ClubRes ) {
+.controller( 'ClubCtrl', function ClubController( $scope, ClubRes, $dialog ) {
   $scope.clubs = ClubRes.query();
+  $scope.gridOptions = {
+    data: 'clubs',
+    columnDefs: [
+      {field: 'id', displayName: 'Id'},
+      {field: 'name', displayName: 'Club Name'},
+      {field: 'contact_officer', displayName: 'Contact Officer'},
+      {displayName: 'Edit', cellTemplate: '<button id="editBtn" type="button" class="btn btn-primary" ng-click="editClub(row.entity)" >Edit</button> '}
+    ],
+    multiSelect: false
+  };
+
+  $scope.editClub = function(club) {
+    $scope.myDialog = $dialog.dialog({dialogFade: false, resolve: {club: function(){return angular.copy(club);}}});
+    $scope.myDialog.open('club/club_edit.tpl.html', 'ClubEditCtrl').then(function(result){
+      if (result === 'cancel'){}
+      else {
+        $scope.clubs = ClubRes.query();
+      }
+    });  
+  };  
+})
+
+/**
+ * We define a controller for the edit action
+ */
+.controller('ClubEditCtrl', function ClubEditController($scope, ClubRes, dialog, club) {
+  $scope.club = club;
+  $scope.submit = function() {
+    $scope.club.$update(function(data) {
+                            dialog.close($scope.club);      
+                          });
+   };
+
+  $scope.cancel = function() {
+    dialog.close('cancel');
+  };
 })
 
 /**
  * Add a resource to allow us to get at the server
  */
 .factory( 'ClubRes', function ( $resource )  {
-  return $resource('../clubs.json');
+  return $resource("../clubs/:id.json", {id:'@id'}, {'update': {method:'PUT'}});
 })
 ;
