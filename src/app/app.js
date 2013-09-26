35544606288ba2af118fb4e9f8caa464fd3189b1
Paul Lambert <paul@planar.id.au>
2013-09-26 21:17:41 +1000

    Fixed page title

--- src/app/app.js
+++ src/app/app.js
@@ -16,5 +16,10 @@ angular.module( 'league', [
 })
 
 .controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
+  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+    if ( angular.isDefined( toState.data.pageTitle ) ) {
+      $scope.pageTitle = toState.data.pageTitle + ' | ngBoilerplate' ;
+    }
+  });
 })
 ;
